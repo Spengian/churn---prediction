8450f4687169fa7040d3a7a72cc432173a986d10
Spentzouris Giannis <132393317+Spengian@users.noreply.github.com>
2026-06-10 13:19:13 +0300

    feat: add database integration

---
+++ database.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+import os
+from datetime import datetime
+
+DATABASE_URL = os.getenv("DATABASE_URL",  "sqlite:///./test.db") #το δευτερο για να το τρεχω τοπικα εκτος docker
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+Session = sessionmaker(engine)
+
+class Base(DeclarativeBase):
+    pass
+
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+class CustomerPred(Base):
+    __tablename__ = "customer_predictions_table"
+
+    id: Mapped[int] = mapped_column(primary_key = True)
+    churn: Mapped[int] = mapped_column()
+    probability: Mapped[float] = mapped_column()
+    timestamp: Mapped[datetime] = mapped_column(server_default=func.now())
